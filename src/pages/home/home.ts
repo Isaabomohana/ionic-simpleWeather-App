a571e11b4538da9d64295b3c521622d5a025fda6
Isaabomohana <mrissaam@outlook.com>
2017-11-03 16:01:17 +0100

    a simple weather app with ionic-cordova-angular

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+export class HomePage {
+  weather: any;
+  location: {
+    city: string,
+    country: string
+  }
+
+  constructor(
+    public navCtrl: NavController,
+    private WeatherProvider: WeatherProvider,
+    private storage: Storage
+  ) {
 
   }
 
+  ionViewWillEnter() {
+    this.storage.get('location').then((val) => {
+      if ( val != null ) {
+        this.location = JSON.parse(val);
+      } else {
+        this.location = {
+          city: 'Berlin',
+          country: 'Germany'
+        }
+      }
+
+      this.WeatherProvider.getWeather(this.location.city, this.location.country).subscribe(weather => {
+        this.weather = weather.current_observation;
+      });
+
+    });
+  }
 }
---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the WeatherProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class WeatherProvider {
+  apiKey = '700d230326c80873';
+  url;
+
+  constructor(public http: Http) {
+    console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.wunderground.com/api/'+this.apiKey+'/conditions/q';
+  }
+
+  getWeather (city, country) {
+    return this.http.get(this.url+'/'+country+'/'+city+'.json')
+      .map(res => res.json());
+  };
+}
